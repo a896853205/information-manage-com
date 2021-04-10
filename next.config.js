e22fefcbbf2a008d2e8ead153e53b49d4c8f6e17
Qccccc's laptop <896853205@qq.com>
2021-04-10 10:14:34 +0800

    fix: 解决根目录babel报错

--- next.config.js
+++ next.config.js
@@ -23,23 +23,27 @@ module.exports = {
 						// Allows for the use of imports
 						sourceType: 'module',
 					},
-					// plugins: ['react'], // '@typescript-eslint', 'react',
+					plugins: ['@typescript-eslint', 'react', 'prettier'],
 					rules: {
 						// Disable prop-types as we use TypeScript for type checking
 						'react/prop-types': 'off',
 						'@typescript-eslint/explicit-function-return-type': 'off',
-						// 'prettier/prettier': 'error',
+						'prettier/prettier': 'error',
 						'@typescript-eslint/interface-name-prefix': 'off',
 						'@typescript-eslint/no-explicit-any': 'off',
 						'@typescript-eslint/ban-ts-ignore': 'off',
 						// needed for NextJS's jsx without react import
 						'react/react-in-jsx-scope': 'off',
-						// array ,
+						// behind last item cannot have comma in array
 						'comma-dangle': [2, 'never'],
+						// single quotes
 						quotes: [2, 'single'],
+						// array left and right cannot have space
 						'array-bracket-spacing': [2, 'never'],
+						// must have semi
 						semi: [2, 'always'],
-						'no-mixed-spaces-and-tabs': [2, false], // 禁止空格和tab混用
+						// use tabs please
+						'no-mixed-spaces-and-tabs': [2, false],
 					},
 				},
 			})
