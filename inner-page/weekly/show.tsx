15750e0a1e7f62290952c006a735cfeb0ffa7c32
iosyyy <626797813@qq.com>
2021-04-17 20:28:54 +0800

    fix:修改样式和规范问题

--- inner-page/weekly/show.tsx
+++ inner-page/weekly/show.tsx
@@ -21,15 +21,35 @@ import 'moment/locale/zh-cn';
 import moment from 'moment';
 import TextArea from 'antd/lib/input/TextArea';
 import Search from 'antd/lib/input/Search';
+import Mock from 'mockjs';
+import { CheckboxValueType } from 'antd/lib/checkbox/Group';
 
 class Show extends Component {
-  // FIXME: 1.把类组件改成函数组件
-  // FIXME: 2.禁止使用any， 不知道类型可以用unknown代替
   constructor(props: {} | Readonly<{}>) {
     super(props);
     let that = this;
+
+    const { data } = Mock.mock({
+      'data|10': [
+        {
+          'key|+1': 1,
+          'index|+1': 1,
+          'name|+1': [
+            '退役锂电材料短程循环与过程污染控制技术与评价方法研究',
+            '电动汽车全生命周期分析与环境评价',
+            '停车相关管理系统及平台',
+            '大型复杂结构施工监控关键技术研究',
+          ],
+          studentName: 'jhx',
+          date: '@date("yyyy/mm")',
+          status: 'false',
+          advice: 'with out advice',
+          state: '指标招生',
+        },
+      ],
+    });
     //初始化行属性
-    const columns: any[] = [
+    const columns = [
       {
         title: '序号',
         key: 'key',
@@ -63,7 +83,7 @@ class Show extends Component {
       {
         title: '操作',
         key: 'action',
-        render: function (_text: any, _record: any, _index: any) {
+        render: function (_text: object, _record: object, _index: number) {
           return (
             <Space>
               <Button
@@ -82,41 +102,22 @@ class Show extends Component {
         },
       },
     ];
-    // FIXME: 3.表格假数据用MOCK数据填充
-    const projectList: any[] = [];
-    for (let i = 0; i < 100; i++) {
-      let randomMonth = Math.floor(Math.random() * 12);
-      let randomYear = Math.floor(Math.random() * 20 + 2000);
-      projectList.push({
-        key: i,
-        studentName: 'jhx' + i,
-        name: '退役锂电材料短程循环与过程污染控制技术与评价方法研究' + i,
-        date:
-          randomYear +
-          '/' +
-          (randomMonth < 10 ? '0' + randomMonth : randomMonth),
-        status: 'success',
-        advice: 'null',
-        index: i,
-      });
-    }
-
-    // FIXME: 4. 这个注释不是这么写的，这个@param是用在function的注释中的
-    /**
-     * @param:data 页面当前显示数据
-     * @param:dataAll 从后台获取的所有数据
-     * @param:columns 当前显示表格的行属性
-     * @param:selectedRowKeys 代表当前选择中的列
-     * @param:loading 代表当前页面正在加载
-     * @param:showModel 是否显示详情的对话框
-     * @param:selectIndex 帮助获取详情对话框的具体参数
-     * @param:selectValue 代表当前正在搜索的value
-     * @param:selectDate 代表当前选择的日期,这里使用moment对象
+    console.log(data);
+    /*
+     * data 页面当前显示数据
+     * dataAll 从后台获取的所有数据
+     * columns 当前显示表格的行属性
+     * selectedRowKeys 代表当前选择中的列
+     * loading 代表当前页面正在加载
+     * showModel 是否显示详情的对话框
+     * selectIndex 帮助获取详情对话框的具体参数
+     * selectValue 代表当前正在搜索的value
+     * selectDate 代表当前选择的日期,这里使用moment对象
      */
 
     this.state = {
-      data: projectList,
-      dataAll: projectList,
+      data: data,
+      dataAll: data,
       columns: columns,
       defaultColumns: columns,
       selectedRowKeys: [], // Check here to configure the default column
@@ -141,43 +142,49 @@ class Show extends Component {
     }, 1000);
   };
 
-  //选中列改变时调用
-  onSelectChange = (selectedRowKeys: any) => {
+  // 选中列改变时调用
+  onSelectChange = (selectedRowKeys: unknown) => {
     this.setState({
       selectedRowKeys,
     });
   };
 
-  //删除选中项
+  // 删除选中项
   delete = () => {
     // @ts-ignore
     let { selectedRowKeys, dataAll } = this.state;
     for (let x of selectedRowKeys) {
-      dataAll = dataAll.filter((_item: any, index: string | number) => {
+      dataAll = dataAll.filter((_item: unknown, index: string | number) => {
         return dataAll[index].key != x;
       });
     }
+    setTimeout(() => {
+      this.setState({
+        loading: true,
+      });
+    }, 1000);
     this.setState({
       dataAll,
+      loading: false,
       data: dataAll,
     });
   };
 
-  //详情页面下载的时候使用
+  // 详情页面下载的时候使用
   downLoad = () => {};
 
-  //设置对话框是否显示
+  // 设置对话框是否显示
   setFalse = () => {
     this.setState({
       showModel: false,
     });
   };
 
-  checkedChange = (e: any) => {
+  checkedChange = (e: CheckboxValueType[]) => {
     // @ts-ignore
     const { defaultColumns } = this.state;
-    let col = defaultColumns.filter((i: { key: any }) => {
-      return e.some((j: any) => i.key == j);
+    let col = defaultColumns.filter((i: { key: number }) => {
+      return e.some((j: CheckboxValueType) => i.key == j);
     });
     this.setState({
       columns: col,
@@ -243,30 +250,34 @@ class Show extends Component {
                 //使用类似于stream的思想通过filter来剔除不符合条件的数据,剔除两次是因为有两个需要做filter的属性
                 const dataFilter = dataAll
                   //剔除搜索框
-                  .filter((_values: any, index: string | number, _a: any) => {
-                    return dataAll[index].name
-                      .toString()
-                      .toLowerCase()
-                      .includes(value.toLowerCase());
-                  })
-                  //剔除日期选择栏
-                  .filter((_values: any, index: string | number, _a: any) => {
-                    if (selectDate) {
-                      return (
-                        selectDate.isSame(
-                          moment(dataAll[index].date, 'YYYY/MM'),
-                          'year'
-                        ) &&
-                        selectDate.isSame(
-                          moment(dataAll[index].date, 'YYYY/MM'),
-                          'month'
-                        )
-                      );
-                    } else {
-                      //如果没选择日期则返回false
-                      return true;
+                  .filter(
+                    (_values: unknown, index: string | number, _a: unknown) => {
+                      return dataAll[index].name
+                        .toString()
+                        .toLowerCase()
+                        .includes(value.toLowerCase());
                     }
-                  });
+                  )
+                  //剔除日期选择栏
+                  .filter(
+                    (_values: unknown, index: string | number, _a: unknown) => {
+                      if (selectDate) {
+                        return (
+                          selectDate.isSame(
+                            moment(dataAll[index].date, 'YYYY/MM'),
+                            'year'
+                          ) &&
+                          selectDate.isSame(
+                            moment(dataAll[index].date, 'YYYY/MM'),
+                            'month'
+                          )
+                        );
+                      } else {
+                        //如果没选择日期则返回false
+                        return true;
+                      }
+                    }
+                  );
                 this.setState({
                   data: dataFilter,
                   selectValue: value,
@@ -276,30 +287,34 @@ class Show extends Component {
             />
             <DatePicker
               onChange={(date, _dateString) => {
-                //同上面的onChange,逻辑上稍有不同
+                // 同上面的onChange,逻辑上稍有不同
                 const dataFilter = dataAll
-                  .filter((_values: any, index: string | number, _a: any) => {
-                    if (date) {
-                      return (
-                        date.isSame(
-                          moment(dataAll[index].date, 'YYYY/MM'),
-                          'year'
-                        ) &&
-                        date.isSame(
-                          moment(dataAll[index].date, 'YYYY/MM'),
-                          'month'
-                        )
-                      );
-                    } else {
-                      return true;
+                  .filter(
+                    (_values: unknown, index: string | number, _a: unknown) => {
+                      if (date) {
+                        return (
+                          date.isSame(
+                            moment(dataAll[index].date, 'YYYY/MM'),
+                            'year'
+                          ) &&
+                          date.isSame(
+                            moment(dataAll[index].date, 'YYYY/MM'),
+                            'month'
+                          )
+                        );
+                      } else {
+                        return true;
+                      }
                     }
-                  })
-                  .filter((_values: any, index: string | number, _a: any) => {
-                    return dataAll[index].name
-                      .toString()
-                      .toLowerCase()
-                      .includes(selectValue.toLowerCase());
-                  });
+                  )
+                  .filter(
+                    (_values: unknown, index: string | number, _a: unknown) => {
+                      return dataAll[index].name
+                        .toString()
+                        .toLowerCase()
+                        .includes(selectValue.toLowerCase());
+                    }
+                  );
                 this.setState({
                   data: dataFilter,
                   selectDate: date,
@@ -329,7 +344,7 @@ class Show extends Component {
           </div>
         </div>
         {/*数据显示信息*/}
-        <div>
+        <div style={{ width: '81vw' }}>
           <Table
             key={data.length}
             rowSelection={rowSelection}
@@ -356,36 +371,55 @@ class Show extends Component {
                 wrapperCol={{ span: 25 }}
                 layout="horizontal"
               >
-                <Form.Item name="studentName" label="姓名" required>
-                  <Input placeholder={data[selectIndex].studentName} />
+                <Form.Item name="studentName" label="姓名">
+                  <div>
+                    <Input
+                      value={data[selectIndex].studentName}
+                      aria-disabled={false}
+                    />
+                  </div>
                 </Form.Item>
                 <Form.Item name="name" label="项目名称">
-                  <Input placeholder={data[selectIndex].name} />
+                  <div>
+                    <Input
+                      aria-disabled={false}
+                      value={data[selectIndex].name}
+                    />
+                  </div>
                 </Form.Item>
                 <Form.Item name="date" label="项目年份">
                   <DatePicker
                     defaultValue={moment(data[selectIndex].date, 'YYYY/MM')}
                     picker={'month'}
+                    disabled={true}
                   />
                 </Form.Item>
                 <Form.Item label="第一周">
-                  <Space>
-                    <Input disabled={false} placeholder="社会实践报告.docx" />
-                    <Button
-                      style={{
-                        marginLeft: '1vh',
-                      }}
-                      type="primary"
-                      danger={true}
-                      onClick={this.downLoad}
-                    >
-                      下载
-                    </Button>
-                  </Space>
+                  <div style={{ width: '50vw', display: 'inline-block' }}>
+                    <Input
+                      disabled={false}
+                      value="社会实践报告.docx"
+                      aria-disabled={false}
+                    />
+                  </div>
+                  <Button
+                    style={{
+                      marginLeft: '1vh',
+                    }}
+                    type="primary"
+                    danger={true}
+                    onClick={this.downLoad}
+                  >
+                    下载
+                  </Button>
                 </Form.Item>
                 <Form.Item label="第二周">
-                  <div style={{ width: 800, display: 'inline-block' }}>
-                    <Input disabled={false} placeholder="社会实践报告.docx" />
+                  <div style={{ width: '50vw', display: 'inline-block' }}>
+                    <Input
+                      disabled={false}
+                      value="社会实践报告.docx"
+                      aria-disabled={false}
+                    />
                   </div>
                   <Button
                     style={{
@@ -399,8 +433,12 @@ class Show extends Component {
                   </Button>
                 </Form.Item>
                 <Form.Item label="第三周">
-                  <div style={{ width: 800, display: 'inline-block' }}>
-                    <Input disabled={false} placeholder="社会实践报告.docx" />
+                  <div style={{ width: '50vw', display: 'inline-block' }}>
+                    <Input
+                      disabled={false}
+                      value="社会实践报告.docx"
+                      aria-disabled={false}
+                    />
                   </div>
                   <Button
                     style={{
@@ -414,8 +452,12 @@ class Show extends Component {
                   </Button>
                 </Form.Item>
                 <Form.Item label="第四周">
-                  <div style={{ width: 800, display: 'inline-block' }}>
-                    <Input disabled={false} placeholder="社会实践报告.docx" />
+                  <div style={{ width: '50vw', display: 'inline-block' }}>
+                    <Input
+                      disabled={false}
+                      value="社会实践报告.docx"
+                      aria-disabled={false}
+                    />
                   </div>
                   <Button
                     style={{
@@ -436,7 +478,7 @@ class Show extends Component {
                 >
                   填写评论
                 </div>
-                <hr /> {/* FIXME: 禁止使用样式标签,antd有专门的分割线Divider */}
+                <hr />
                 <Form.Item name="advice">
                   <Radio.Group defaultValue={2}>
                     <Radio value={1}>优秀</Radio>
