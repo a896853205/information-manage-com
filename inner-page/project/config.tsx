5449451cdc6de100908255f644603c85d45a36a8
renhanbin <1870012027@qq.com>
2021-04-19 22:05:11 +0800

    fix: 修复form样式

--- inner-page/project/config.tsx
+++ inner-page/project/config.tsx
@@ -9,14 +9,41 @@ const Config = () => {
   const [isShow, { setTrue, setFalse }] = useBoolean(false);
   return (
     <>
-      <Button
-        onClick={setTrue}
-        type="primary"
-        style={{ marginRight: 10 }}
-        icon={<CopyOutlined />}
-      >
-        导出
-      </Button>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Button
+          onClick={setTrue}
+          type="primary"
+          style={{ marginRight: 10 }}
+          icon={<CopyOutlined />}
+        >
+          导出
+        </Button>
+        <Space>
+          项目年份：
+          <Select
+            defaultValue="全部"
+            style={{ width: 120 }}
+            onChange={() => {}}
+          >
+            <Option value="全部">全部</Option>
+            <Option value="2019">2019</Option>
+            <Option value="2018">2018</Option>
+            <Option value="2020">2020</Option>
+          </Select>
+          项目状态：
+          <Select
+            defaultValue="全部"
+            style={{ width: 120 }}
+            onChange={() => {}}
+          >
+            <Option value="全部">全部</Option>
+            <Option value="审稿">审稿</Option>
+            <Option value="审批中">审批中</Option>
+            <Option value="未通过">未通过</Option>
+          </Select>
+          <Search placeholder="搜索" style={{ width: 200 }} />
+        </Space>
+      </div>
       <Modal
         title="选择需要导出的类型"
         visible={isShow}
@@ -31,7 +58,6 @@ const Config = () => {
           </Select>
         </Space>
       </Modal>
-      <Search placeholder="搜索" style={{ width: 200 }} />
     </>
   );
 };
