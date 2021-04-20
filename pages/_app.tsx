6e636d5ed42c0d68e0056ad7eeae568bcab06351
Qccccc's laptop <896853205@qq.com>
2021-04-20 20:57:51 +0800

    feat: antd locale provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import { ConfigProvider } from 'antd';
+import locale from 'antd/lib/locale/zh_CN';
+import 'dayjs/locale/zh-cn';
+
 import 'styles/antd.css';
 import 'styles/layout.css';
 import 'styles/page_loading_style.css';
@@ -7,10 +11,13 @@ import 'styles/page_loading_style.css';
 const MyApp = (props: any) => {
   const { Component, pageProps } = props;
   const Layout = Component.Layout ? Component.Layout : React.Fragment;
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ConfigProvider locale={locale}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ConfigProvider>
   );
 };
 
