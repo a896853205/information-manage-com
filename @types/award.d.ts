60c1acdde9127c2c2bd30587de9b28bf846dc554
123nian <2872713038@qq.com>
2021-04-22 23:35:08 +0800

    feat:增加了奖励审批功能

---
+++ @types/award.d.ts
@@ -0,0 +1,20 @@
+/**
+ * 研究生培养命名空间
+ */
+ namespace PT {
+    /**
+     * 学生
+     */
+    type award = {
+      key: string;
+      name: string;
+      schoolName: string;
+      
+      
+      type: string;
+  
+      date: number;
+      state: string;
+    };
+  }
+  
---
+++ inner-page/enterprise-manager-award/compose.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+
+import Mock from 'mockjs';
+
+import Show from 'inner-page/enterprise-manager-award/show';
+import Config from 'inner-page/enterprise-manager-award/config';
+const Compose = () => {
+  const [awardData, setData] = useState<PT.award[]>([]); // data 页面当前显示数据
+
+  useEffect(() => {
+    const { awardData } = Mock.mock({
+      'awardData|10': [
+        {
+          'key|+1': 1,
+          'index|+1': 0,
+          'schoolName|+1': ['中山大学', '广东工业大学'],
+          'name|+1': '@cname()', // 生成随机中文名字
+          'date|+1': '@datetime()',
+          'state|+1': ['已审批', '未审批'],
+          'type|+1': ['专利型奖励', '科研型奖励'],
+        },
+      ],
+    });
+
+    // 初始化数据属性
+    setData(awardData);
+  }, []);
+  return (
+    <>
+      {/* <BreadcrumbList /> */}
+      <Config />
+      <Show awarddata={awardData} />
+    </>
+  );
+};
+
+export default Compose;
