05db2e7405a7dd451aa2dafc82ef6e7952f807c2
renhanbin <1870012027@qq.com>
2021-04-24 22:00:39 +0800

    feat: add enterprise manager user check

--- pages/enterprise-manager-user-check.tsx
+++ pages/enterprise-manager-user-check.tsx
@@ -1,11 +1,16 @@
 import MyLayout from 'layouts/index';
+import Compose from 'inner-page/enterprise-manager-user-check/compose';
 
 /**
  * 角色：企业管理员
  * 信息审批管理->用户账号审批
  */
 const UserCheck = () => {
-  return <></>;
+  return (
+    <>
+      <Compose />
+    </>
+  );
 };
 UserCheck.Layout = MyLayout;
 
