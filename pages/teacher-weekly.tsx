abac622fb73242bc89ab69ed7cc719037cbae5ec
Aurora-NB <772335766@qq.com>
2021-04-27 21:54:46 +0800

    feat:增加了高校导师周志管理页面

--- pages/teacher-weekly.tsx
+++ pages/teacher-weekly.tsx
@@ -1,11 +1,12 @@
 import MyLayout from 'layouts/index';
+import Compose from 'inner-page/teacher-weekly/compose';
 
 /**
  * 角色：高校导师
  * 教务信息管理->周志管理
  */
 const Weekly = () => {
-  return <></>;
+  return <Compose></Compose>;
 };
 Weekly.Layout = MyLayout;
 
