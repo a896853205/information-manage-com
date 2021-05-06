acec543e46fe2ca2dbf7bce0d37d3203ff3d276b
Qccccc's laptop <896853205@qq.com>
2021-05-06 14:54:19 +0800

    feat: add github button

--- inner-page/index/index.tsx
+++ inner-page/index/index.tsx
@@ -1,38 +1,14 @@
-import { useCallback, useState } from 'react';
-
-import Login from 'inner-page/index/login';
-import Register from 'inner-page/index/register';
-
-import styles from './index.module.scss';
-
-/** index页处于登录状态 */
-export const LOGIN = 'login';
-/** index页处于注册状态 */
-export const REGISTER = 'register';
+import { Button } from 'antd';
+import { GithubOutlined } from '@ant-design/icons';
 
 /**
  * show
  */
 const Show = () => {
-  const [pageState, setPageState] = useState(LOGIN);
-  const onPageStateChange = useCallback<(pageState: string) => void>(
-    pageState => {
-      setPageState(pageState);
-    },
-    []
-  );
-
   return (
-    <div className={styles['index-box']}>
-      <div className={styles['index-inner-box']}>
-        {pageState === LOGIN ? (
-          <Login onPageStateChange={onPageStateChange} />
-        ) : null}
-        {pageState === REGISTER ? (
-          <Register onPageStateChange={onPageStateChange} />
-        ) : null}
-      </div>
-    </div>
+    <Button type="primary" icon={<GithubOutlined />} size="large" shape="round">
+      login
+    </Button>
   );
 };
 
