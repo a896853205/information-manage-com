f80b7898d6f2901bc74a7c9e1acfcefa6d808936
Qccccc's laptop <896853205@qq.com>
2021-05-07 16:14:41 +0800

    feat: finish account-list layout

--- inner-page/account-list/index.tsx
+++ inner-page/account-list/index.tsx
@@ -1,5 +1,18 @@
+import { Typography } from 'antd';
+
+import Table from './table';
+import Search from './search';
+
+const { Title } = Typography;
+
 const AccountList = () => {
-  return <>account-list</>;
+  return (
+    <Typography>
+      <Title>account</Title>
+      <Search />
+      <Table />
+    </Typography>
+  );
 };
 
 export default AccountList;
--- inner-page/message-list/index.tsx
+++ inner-page/message-list/index.tsx
@@ -1,4 +1,4 @@
-import { Typography, Space } from 'antd';
+import { Typography } from 'antd';
 
 import Table from './table';
 import Search from './search';
@@ -6,16 +6,12 @@ import Search from './search';
 const { Title } = Typography;
 
 const MessageList = () => {
-  // TODO: 上面有搜索，Key/Message查询，type有筛选，Action操作（删除与修改）
-
   return (
-    <>
-      <Typography>
-        <Title>message-list</Title>
-        <Search />
-        <Table />
-      </Typography>
-    </>
+    <Typography>
+      <Title>message</Title>
+      <Search />
+      <Table />
+    </Typography>
   );
 };
 
