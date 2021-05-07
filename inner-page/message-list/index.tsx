2fe5c0cee7d44f6f0714381ea50c49a2c7691907
Qccccc's laptop <896853205@qq.com>
2021-05-07 14:54:24 +0800

    feat: message-list table

--- inner-page/message-list/index.tsx
+++ inner-page/message-list/index.tsx
@@ -1,5 +1,23 @@
+import { Typography, Table } from 'antd';
+
+const { Column } = Table;
+
+const { Title } = Typography;
 const MessageList = () => {
-  return <>message-list</>;
+  // TODO: 上面有搜索，Key/Message查询，type有筛选，Action操作（删除与修改）
+  return (
+    <>
+      <Typography>
+        <Title>message-list</Title>
+        <Table>
+          <Column title="Key" dataIndex="key" key="key" />
+          <Column title="Message" dataIndex="message" key="message" />
+          <Column title="Type" dataIndex="type" key="type" />
+          <Column title="Action" />
+        </Table>
+      </Typography>
+    </>
+  );
 };
 
 export default MessageList;
